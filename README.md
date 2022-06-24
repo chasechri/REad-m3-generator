bb52874b51b8b4f1da4a17fc8eb712c61fa9f687
chasechri <christensonchase451@gmail.com>
2022-06-24 11:10:47 -0500

    readme test

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# REad-m3-generator
+    #title
+
+    *[description](#description)
+    *[installation](#installation)
+    *[usage](#usage)
+    *[contribution](#contribution)
+    *[instructions](#instructions)
+    *[license](#license)
+    ##description
+    ##installation
+    ##usage
+    ##contribution
+    ##instructions
+    ##license
+
+    #Contact
+    * GitHub dev
+    * Email chase
--- develop/utils/index.js
+++ develop/utils/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = (readmeData) => {
+  console.log(`
+  README information 
+  `);
+  return prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please enter the title of your project",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
